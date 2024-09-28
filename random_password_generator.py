9a174706ec1e91f5ba9ff22010c33b7435725023
Pedro Paulo de Jesus Presidio <158526499+ppresidio@users.noreply.github.com>
2024-09-27 23:53:36 -0600

    Add files via upload

---
+++ random_password_generator.py
@@ -0,0 +1,20 @@
+"""
+Author: Pedro Presidio
+Name: random_password_generator.py
+Date: 28.9.2024
+Purpose: Generates a random password for the user
+"""
+
+#TODO: import 
+#TODO: Define character sets for password generation
+# TODO: Add character sets based on user preferences (digits, uppercase, special characters)
+# TODO: Ensure the password contains at least one character from each selected category
+# TODO: Fill the rest of the password with random characters
+# TODO: Shuffle the password to ensure randomness
+# TODO: Prompt the user for password length and validate the input
+# TODO: Ask user if they want to include digits, uppercase letters, and special characters
+# TODO: Ensure the user selects at least one character set (digits, uppercase, or special characters)
+# TODO: Generate and display the password
+# TODO: Offer the option to save the password to a file
+# TODO: Save the password to a text file
+# TODO: Run the main function
